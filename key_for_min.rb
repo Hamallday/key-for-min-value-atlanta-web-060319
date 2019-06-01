2ac11201c8a1d07147fda8777cbf614bc1d7afc0
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-06-01 22:32:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,18 @@
 
 def key_for_min_value(name_hash)
 
+  def key_for_min_value(name_hash)
+  # lowest_key = nil
+  # lowest_value = nil
+  name_hash.each do |key, value|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = value
+      lowest_key = key 
+    end
+  end
+  lowest_key
+end
+
+key_for_min_value(name_hash)
+
 end
